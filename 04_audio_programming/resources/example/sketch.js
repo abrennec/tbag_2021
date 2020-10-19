7b95927f4ef4b177db4102271c936e4186be9b00
Markus Traber <markus@fuchsteufels.de>
2020-10-19 18:18:31 +0200

    Snapshot

--- 04_audio_programming/resources/example/sketch.js
+++ 04_audio_programming/resources/example/sketch.js
@@ -5,15 +5,15 @@
 let sample_cow, sample_glass, sample_slamming;
 let button_cow, button_glass, button_slamming;
 let radio_playmode_cow, radio_playmode_glass, radio_playmode_slamming;
+let slider_panning_cow, slider_panning_glass, slider_panning_slamming;
 
 function setup() {
   // Loading the sound samples
   loadSamples();
 
-  // Creating buttons, to play the sound
+  // Setting up the UI
   setupUI();
-    
-
+  
   // Create and setup canvas
   //createCanvas(720, 200);
   //background(255, 0, 0);
@@ -27,34 +27,56 @@ function loadSamples() {
 
 function setupUI() {
   // Buttons
-  button_cow = createButton('Cow');
-  button_cow.position(10 , 10);
-  button_cow.mousePressed(function(){
-    buttonPressed(sample_cow);
-  });
-
-  button_glass = createButton('Glass');
-  button_glass.position(10 , 40);
-  button_glass.mousePressed(function(){
-    buttonPressed(sample_glass);
-  });
-
-  button_slamming = createButton('Slamming');
-  button_slamming.position(10 , 70);
-  button_slamming.mousePressed(function(){
-    buttonPressed(sample_slamming);
-  });
+  createSampleButton(button_cow, sample_cow, 'Cow', 10, 10);
+  createSampleButton(button_glass, sample_glass, 'Glass', 10, 40);
+  createSampleButton(button_slamming, sample_slamming, 'Slamming', 10, 70);
 
   // Radio buttons for playing mode
-  createSpan('Play Mode: ').position(120,10);
-  radio_playmode_cow = createRadio('Test');
-  radio_playmode_cow.position(200, 10);
-  addPlayModeOptions(radio_playmode_cow);
+  createPlayModeRadioButton(radio_playmode_cow, sample_cow, 120, 10);
+  createPlayModeRadioButton(radio_playmode_glass, sample_glass, 120, 40);
+  createPlayModeRadioButton(radio_playmode_slamming, sample_slamming, 120, 70);
+
+  // Slider element for panning
+  createPanningSlider(slider_panning_cow, sample_cow, 350, 10);
+  createPanningSlider(slider_panning_glass, sample_glass, 350, 40);
+  createPanningSlider(slider_panning_slamming, sample_slamming, 350, 70);
 }
 
-function addPlayModeOptions(radioElement) {
-  radioElement.option('restart');
-  radioElement.option('sustain');
+function createSampleButton(btn, sample, label, posX, posY) {
+  btn = createButton(label);
+  btn.position(posX , posY);
+
+  // When mouse is being pressed (on button), play sample
+  btn.mousePressed(function() {
+    sample.play();
+  });
+}
+
+function createPlayModeRadioButton(radioBtn, sample, posX, posY) {
+  createSpan('Play Mode: ').position(posX, posY);
+  radioBtn = createRadio('radio_' + posY);
+  radioBtn.position(posX + 80, posY);
+  radioBtn.option('restart');
+  radioBtn.option('sustain');
+  
+  radioBtn.selected('restart');
+  sample.playMode(radioBtn.value());
+
+  // When mouse has been clicked (after releasing mouse button), set the play mode
+  radioBtn.mouseClicked(function() {
+    sample.playMode(radioBtn.value());
+  });
+}
+
+function createPanningSlider(panningSlider, sample, posX, posY) {
+  createSpan('Panning: ').position(posX, posY);
+  panningSlider = createSlider(-1, 1, 0, 0);
+  panningSlider.position(posX + 60, posY);
+
+   // When mouse has been clicked (after releasing mouse button), set the panning
+  panningSlider.mouseClicked(function() {
+    sample.pan(panningSlider.value());
+  });
 }
 
 // function draw() {
@@ -71,12 +93,3 @@ function addPlayModeOptions(radioElement) {
 //     background(0, 255, 0);
 //   }
 // }
-
-function buttonPressed(sample) {
-  if (sample.isPlaying()) {
-    // .isPlaying() returns a boolean
-    sample.stop();
-  } else {
-    sample.play();
-  }
-}
